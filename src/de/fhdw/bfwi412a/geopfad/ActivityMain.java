63d7050ae52735de6ac4d649a8e17758f3c8a54e
Matthias <gordon.matthias@googlemail.com>
2014-07-04 17:21:58 +0200

    Jetzt auch noch der src Ordner, der vorher irgendwie nicht mitgenommen wurde.

---
+++ src/de/fhdw/bfwi412a/geopfad/ActivityMain.java
@@ -0,0 +1,13 @@
+package de.fhdw.bfwi412a.geopfad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
