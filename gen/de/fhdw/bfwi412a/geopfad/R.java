684ed31d689a81ea80f6d3ed17658d29f165ce18
Matthias <gordon.matthias@googlemail.com>
2014-07-06 19:44:17 +0200

    Implementierung eines "Swipe Tab-Menüs". Bisher befindet sich die komplette Programmierung in den Klassen ActivityMain, FragmentA und FragmentB. Das muss natürlich noch logisch aufgeteilt werden.

--- gen/de/fhdw/bfwi412a/geopfad/R.java
+++ gen/de/fhdw/bfwi412a/geopfad/R.java
@@ -13,8 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int pager=0x7f060000;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int fragment_a=0x7f030001;
+        public static final int fragment_b=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
---
+++ src/de/fhdw/bfwi412a/geopfad/FragmentA.java
@@ -0,0 +1,18 @@
+package de.fhdw.bfwi412a.geopfad;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentA extends Fragment {
+
+	public FragmentA () {
+		
+	}
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_a, container, false);
+	}
+}
