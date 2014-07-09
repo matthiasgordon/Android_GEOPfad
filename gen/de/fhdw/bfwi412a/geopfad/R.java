4c989873a592898e78d18bfb64339b6a04a43be9
MarcN04 <marc-niedermeier@web.de>
2014-07-09 14:01:54 +0200

    Noch ein neuer String...

--- gen/de/fhdw/bfwi412a/geopfad/R.java
+++ gen/de/fhdw/bfwi412a/geopfad/R.java
@@ -22,6 +22,8 @@ public final class R {
         public static final int fragment_b=0x7f030002;
     }
     public static final class string {
+        public static final int KKK=0x7f040004;
+        public static final int Popel=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int profi=0x7f040002;
