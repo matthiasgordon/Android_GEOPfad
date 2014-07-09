b4a350bd132ba72ad2266bf56faeb75dcb36d7d6
hungry7 <matthias@compicture.de>
2014-07-09 13:02:33 +0200

    Programmierprofi :)

--- gen/de/fhdw/bfwi412a/geopfad/R.java
+++ gen/de/fhdw/bfwi412a/geopfad/R.java
@@ -24,6 +24,7 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int profi=0x7f040002;
     }
     public static final class style {
         /** 
