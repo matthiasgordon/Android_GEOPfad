8c2df0805a4d52e4078801b96c7d35d3d36275b2
matthiasgordon <gordon.matthias@googlemail.com>
2014-07-10 16:16:58 +0200

    Grundstruktur der Klasse ActivityLocation erstellt.

---
+++ src/de/fhdw/bfwi412a/geopfad/ActivityLocations.java
@@ -0,0 +1,32 @@
+package de.fhdw.bfwi412a.geopfad;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityLocations extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initData();
+		initGUI();
+		initApplicationLogic();
+		initEventToListenerMapping();
+	}
+	
+	private void initData () {
+		
+	}
+
+	private void initGUI () {
+		
+	}
+	
+	private void initApplicationLogic () {
+		
+	}
+	
+	private void initEventToListenerMapping () {
+		
+	}
+}
