4f99c2a3a9cb9153ca5ccc2f3083669936a7853c
PGlawe <patrickglawe@arcor.de>
2014-07-10 15:00:16 +0200

    zweiter Versuch

--- gen/de/fhdw/bfwi412a/geopfad/R.java
+++ gen/de/fhdw/bfwi412a/geopfad/R.java
@@ -29,6 +29,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int profi=0x7f040002;
+        public static final int zweiter Versuch=0x7f040007;
     }
     public static final class style {
         /** 
