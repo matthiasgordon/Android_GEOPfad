580cd857f4ba062dc3d6589e3327be6b7fdebf66
MarcN04 <marc-niedermeier@web.de>
2014-07-14 21:19:49 +0200

    MapFragment zu FragmentA hinzugefügt.

---
+++ src/de/fhdw/bfwi412a/geopfad/Coordinates.java
@@ -0,0 +1,130 @@
+package de.fhdw.bfwi412a.geopfad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Coordinates {
+
+	// Klasse um die Klasse Fragment A übersichtlicher zu gestalten
+	
+	List <LatLng> mCoordinates = new ArrayList <LatLng> ();
+	
+	private final LatLng STEP_1 = new LatLng(50.99029967, 7.13201523);
+	private final LatLng STEP_2 = new LatLng(50.99202848, 7.13038445);
+	private final LatLng STEP_3 = new LatLng(50.99232561, 7.13098526);
+	private final LatLng STEP_4 = new LatLng(50.99320349, 7.13195086);
+	private final LatLng STEP_5 = new LatLng(50.99368969, 7.13313103);
+	private final LatLng STEP_6 = new LatLng(50.99582353, 7.13476181);
+	private final LatLng STEP_7 = new LatLng(50.99701195, 7.13579178);
+	private final LatLng STEP_8 = new LatLng(50.99888905, 7.13467598);
+	private final LatLng STEP_9 = new LatLng(51.00015842, 7.13562012);
+	private final LatLng STEP_10 = new LatLng(51.00036097, 7.13639259);
+	private final LatLng STEP_11 = new LatLng(51.0012252, 7.13855982);
+	private final LatLng STEP_12 = new LatLng(51.00214342, 7.13866711);
+	private final LatLng STEP_13 = new LatLng(51.00256202, 7.13894606);
+	private final LatLng STEP_14 = new LatLng(51.00322366, 7.14139223);
+	private final LatLng STEP_15 = new LatLng(51.00431737, 7.14370966);
+	private final LatLng STEP_16 = new LatLng(51.00414184, 7.14529753);
+	private final LatLng STEP_17 = new LatLng(51.00408783, 7.14652061);
+	private final LatLng STEP_18 = new LatLng(51.00319665, 7.14866638);
+	private final LatLng STEP_19 = new LatLng(51.00321016, 7.14943886);
+	private final LatLng STEP_20 = new LatLng(51.00384478, 7.151649);
+	private final LatLng STEP_21 = new LatLng(51.00361524, 7.15289354);
+	private final LatLng STEP_22 = new LatLng(51.00389879, 7.15368748);
+	private final LatLng STEP_23 = new LatLng(51.00370976, 7.15392351);
+	private final LatLng STEP_24 = new LatLng(51.00352072, 7.15325832);
+	private final LatLng STEP_25 = new LatLng(51.00377727, 7.15158463);
+	private final LatLng STEP_26 = new LatLng(51.00438489, 7.15160608);
+	private final LatLng STEP_27 = new LatLng(51.00474945, 7.15381622);
+	private final LatLng STEP_28 = new LatLng(51.00418235, 7.15579033);
+	private final LatLng STEP_29 = new LatLng(51.00365575, 7.15559721);
+	private final LatLng STEP_30 = new LatLng(51.00218393, 7.15349436);
+	private final LatLng STEP_31 = new LatLng(51.00241348, 7.15501785);
+	private final LatLng STEP_32 = new LatLng(51.0012387, 7.1562624);
+	private final LatLng STEP_33 = new LatLng(51.00175183, 7.15802193);
+	private final LatLng STEP_34 = new LatLng(51.00171132, 7.15978146);
+	private final LatLng STEP_35 = new LatLng(51.00149527, 7.16016769);
+	private final LatLng STEP_36 = new LatLng(51.00114418, 7.16012478);
+	private final LatLng STEP_37 = new LatLng(51.00099564, 7.162292);
+	private final LatLng STEP_38 = new LatLng(51.00064455, 7.16085434);
+	private final LatLng STEP_39 = new LatLng(51.000469, 7.15950251);
+	private final LatLng STEP_40 = new LatLng(50.99953724, 7.15875149);
+	private final LatLng STEP_41 = new LatLng(50.99853795, 7.1568203);
+	private final LatLng STEP_42 = new LatLng(50.99733606, 7.15630531);
+	private final LatLng STEP_43 = new LatLng(50.99525632, 7.15812922);
+	private final LatLng STEP_44 = new LatLng(50.9949457, 7.15574741);
+	private final LatLng STEP_45 = new LatLng(50.99571549, 7.15381622);
+	private final LatLng STEP_46 = new LatLng(50.99552642, 7.1498251);
+	private final LatLng STEP_47 = new LatLng(50.9939463, 7.14497566);
+	private final LatLng STEP_48 = new LatLng(50.99374372, 7.14484692);
+	private final LatLng STEP_49 = new LatLng(50.99312246, 7.14128494);
+	private final LatLng STEP_50 = new LatLng(50.99336556, 7.13592052);
+	private final LatLng STEP_51 = new LatLng(50.99291987, 7.13589907);
+	private final LatLng STEP_52 = new LatLng(50.99289286, 7.13353872);
+	private final LatLng STEP_53 = new LatLng(50.99239314, 7.13255167);
+	private final LatLng STEP_54 = new LatLng(50.9929874, 7.13173628);
+	
+	public Coordinates (){
+		mCoordinates.add(STEP_1);
+		mCoordinates.add(STEP_2);
+		mCoordinates.add(STEP_3);
+		mCoordinates.add(STEP_4);
+		mCoordinates.add(STEP_5);
+		mCoordinates.add(STEP_6);
+		mCoordinates.add(STEP_7);
+		mCoordinates.add(STEP_8);
+		mCoordinates.add(STEP_9);
+		mCoordinates.add(STEP_10);
+		mCoordinates.add(STEP_11);
+		mCoordinates.add(STEP_12);
+		mCoordinates.add(STEP_13);
+		mCoordinates.add(STEP_14);
+		mCoordinates.add(STEP_15);
+		mCoordinates.add(STEP_16);
+		mCoordinates.add(STEP_17);
+		mCoordinates.add(STEP_18);
+		mCoordinates.add(STEP_19);
+		mCoordinates.add(STEP_20);
+		mCoordinates.add(STEP_21);
+		mCoordinates.add(STEP_22);
+		mCoordinates.add(STEP_23);
+		mCoordinates.add(STEP_24);
+		mCoordinates.add(STEP_25);
+		mCoordinates.add(STEP_26);
+		mCoordinates.add(STEP_27);
+		mCoordinates.add(STEP_28);
+		mCoordinates.add(STEP_29);
+		mCoordinates.add(STEP_30);
+		mCoordinates.add(STEP_31);
+		mCoordinates.add(STEP_32);
+		mCoordinates.add(STEP_33);
+		mCoordinates.add(STEP_34);
+		mCoordinates.add(STEP_35);
+		mCoordinates.add(STEP_36);
+		mCoordinates.add(STEP_37);
+		mCoordinates.add(STEP_38);
+		mCoordinates.add(STEP_39);
+		mCoordinates.add(STEP_40);
+		mCoordinates.add(STEP_41);
+		mCoordinates.add(STEP_42);
+		mCoordinates.add(STEP_43);
+		mCoordinates.add(STEP_44);
+		mCoordinates.add(STEP_45);
+		mCoordinates.add(STEP_46);
+		mCoordinates.add(STEP_47);
+		mCoordinates.add(STEP_48);
+		mCoordinates.add(STEP_49);
+		mCoordinates.add(STEP_50);
+		mCoordinates.add(STEP_51);
+		mCoordinates.add(STEP_52);
+		mCoordinates.add(STEP_53);
+		mCoordinates.add(STEP_54);
+	}
+
+	public List<LatLng> getCoordinates() {
+		return mCoordinates;
+	}
+	
+}
--- src/de/fhdw/bfwi412a/geopfad/FragmentA.java
+++ src/de/fhdw/bfwi412a/geopfad/FragmentA.java
@@ -2,17 +2,45 @@ package de.fhdw.bfwi412a.geopfad;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class FragmentA extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
+public class FragmentA extends Fragment {
+	
+	private final LatLng LOCATION_GEOPFAD = new LatLng(50.99876752, 7.14546919);
+	
+	SupportMapFragment mFragment;
+	GoogleMap mMap;
+	
 	public FragmentA () {
 		
 	}
 	
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_a, container, false);
+		View mView = inflater.inflate(R.layout.fragment_a, container, false);
+		
+		FragmentManager mFragmentManager = getActivity().getSupportFragmentManager();
+		
+		if(mFragment == null){
+			mFragment = (SupportMapFragment) mFragmentManager.findFragmentById(R.id.map);
+		}
+		
+		mMap = mFragment.getMap();
+		mMap.addPolyline(new PolylineOptions()
+					.addAll(new Coordinates().getCoordinates())
+					.width(8));
+		mMap.addMarker(new MarkerOptions().position(LOCATION_GEOPFAD).title("Mitte der Karte!"));
+		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LOCATION_GEOPFAD, 14));
+		mMap.setMyLocationEnabled(true);
+		return mView;
 	}
 }
