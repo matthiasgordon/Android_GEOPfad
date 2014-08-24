76910094ac916ca9e165b3eb936e77715271c377
plaktron <mar_boettcher@web.de>
2014-08-24 16:01:41 +0200

    fehlende Activity nachgeladen

---
+++ src/de/fhdw/bfwi412a/geopfad/ListFragment.java
@@ -0,0 +1,75 @@
+package de.fhdw.bfwi412a.geopfad;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+	
+	ListFragmentData mData;
+	ListFragmentGUI mGUI;
+	ListFragmentApplicationLogic mAppLogic;
+	ListFragmentEventToListenerMapping mEventToListenerMapping;
+
+	public ListFragment () {
+		
+	}
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
+		View mView = inflater.inflate(R.layout.list_fragment, container, false);
+		setHasOptionsMenu(true);
+		initData();
+		initGUI(mView);
+		initApplicationLogic();
+		initEventToListenerMapping();
+		return mView;
+	}
+
+	private void initData() {
+		mData = new ListFragmentData(this);
+	}
+	
+	private void initGUI(View mView) {
+		mGUI = new ListFragmentGUI(mView, mData);
+	}
+	
+	private void initApplicationLogic() {
+		mAppLogic = new ListFragmentApplicationLogic(mData, mGUI);
+	}
+	
+	private void initEventToListenerMapping() {
+		mEventToListenerMapping = new ListFragmentEventToListenerMapping(mGUI, mAppLogic);
+	}
+
+	public void addLocation() {
+		Intent intent = new Intent(getActivity(), AddLocation.class);
+		startActivity(intent);
+	}
+	
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		// TODO Auto-generated method stub
+		inflater.inflate(R.menu.location_actionbar_menu, menu);
+		super.onCreateOptionsMenu(menu, inflater);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.addLocation:
+	            addLocation();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+}
