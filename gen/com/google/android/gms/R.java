d585b4affea7b2c8816ffe802524e7eb5b1dcded
PGlawe <patrickglawe@arcor.de>
2014-08-27 10:30:03 +0200

    Bilder hochladen

--- gen/com/google/android/gms/R.java
+++ gen/com/google/android/gms/R.java
@@ -99,8 +99,8 @@ public final class R {
 		public static final int ic_plusone_small_off_client = 0x7f020029;
 		public static final int ic_plusone_standard_off_client = 0x7f02002a;
 		public static final int ic_plusone_tall_off_client = 0x7f02002b;
-		public static final int powered_by_google_dark = 0x7f02002d;
-		public static final int powered_by_google_light = 0x7f02002e;
+		public static final int powered_by_google_dark = 0x7f02002e;
+		public static final int powered_by_google_light = 0x7f02002f;
 	}
 	public static final class id {
 		public static final int book_now = 0x7f090010;
